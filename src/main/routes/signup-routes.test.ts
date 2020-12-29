d95bf62dff6471e2ebc967f360a3c669463a8a24
Daniel Otaviano <danielpontes0@icloud.com>
2020-12-29 20:04:40 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+/* eslint-disable jest/expect-expect */
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Daniel',
+        email: 'danielotavianobp@gmail.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
+      .expect(200)
+  })
+})
