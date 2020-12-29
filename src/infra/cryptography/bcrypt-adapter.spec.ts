28fb4c612fa6ce725e14851bacf4f28b6e3e1433
Daniel Otaviano <danielpontes0@icloud.com>
2020-12-29 12:12:40 -0300

    fix: fix name of the test

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 describe('Bcrypt Adapter', () => {
-  test('shouldShould call bcrypt with correct values', async () => {
+  test('Should call bcrypt with correct values', async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
