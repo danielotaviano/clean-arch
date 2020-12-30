5341cbc1ee236148ae072c2012d83a0010ff2e20
Daniel Otaviano <danielpontes0@icloud.com>
2020-12-29 23:26:19 -0300

    refactor: connect to mongodb before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,8 +1,22 @@
 /* eslint-disable jest/expect-expect */
+import { MongoMemoryServer } from 'mongodb-memory-server'
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  const mongod = new MongoMemoryServer()
+
+  beforeAll(async () => {
+    const uri = await mongod.getUri()
+
+    await MongoHelper.connect(uri)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongod.stop()
+  })
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
