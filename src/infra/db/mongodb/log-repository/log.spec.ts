2fe53f11b11d08412f5fb9dce1c2e17087b3cb41
Daniel Otaviano <danielpontes0@icloud.com>
2020-12-31 10:27:30 -0300

    refactor: move sut creation to a factory methods

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -3,6 +3,10 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LogMongoRepository } from './log'
 
+const makeSut = ():LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
 describe('Log Mongo Repository', () => {
   const mongod = new MongoMemoryServer()
   let errorCollection: Collection
@@ -23,7 +27,7 @@ describe('Log Mongo Repository', () => {
     await errorCollection.deleteMany({})
   })
   test('Should create an error log on sucess', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
