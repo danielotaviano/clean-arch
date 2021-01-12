9b52f5c8399893991f9697bbafadbd475fa29519
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-12 20:46:19 -0300

    refactor: move data layer interfaces to folders

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,13 +1,13 @@
 import { Authentication, AuthRequest } from '../../../domain/usecases/authentication'
-import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+  constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
   }
 
-  async auth ({ email, password }: AuthRequest): Promise<string> {
+  async auth({ email, password }: AuthRequest): Promise<string> {
     await this.loadAccountByEmailRepository.load(email)
     return null
   }
