ac107533c52bdfa390f33602652aec3ad389da76
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-12 21:04:34 -0300

    test: ensure DbAuthentication returns null if HashCompare returns false

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -87,4 +87,12 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuth())
     await expect(promise).rejects.toThrow()
   })
+  test('Should return null if HashCompare returns false', async () => {
+    const { sut, hashCompareStub } = makeSut()
+    jest.spyOn(hashCompareStub, 'compare').mockReturnValueOnce(
+      new Promise(resolve => resolve(false))
+    )
+    const accessToken = await sut.auth(makeFakeAuth())
+    expect(accessToken).toBeNull()
+  })
 })
