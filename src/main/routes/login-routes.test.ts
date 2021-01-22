671cd04b935ab3ab2ac03c92909f9d5550135cf8
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-22 07:49:53 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,8 +1,12 @@
 /* eslint-disable jest/expect-expect */
+import { Collection } from 'mongodb'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   const mongod = new MongoMemoryServer()
@@ -19,7 +23,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -32,6 +36,24 @@ describe('Login Routes', () => {
           email: 'danielotavianobp@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Daniel',
+        email: 'danielotavianobp@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'danielotavianobp@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
