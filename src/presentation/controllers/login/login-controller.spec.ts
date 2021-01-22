606aad22ae1e4d86b2ea2e2c88f7a9fe990404ba
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-22 07:30:43 -0300

    feat: add LoginFactory

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -39,7 +39,7 @@ interface SutType {
 const makeSut = () :SutType => {
   const authenticationStub = makeAuthentication()
   const validationStub = makeValidation()
-  const sut = new LoginController(validationStub, authenticationStub)
+  const sut = new LoginController(authenticationStub, validationStub)
 
   return {
     sut,
