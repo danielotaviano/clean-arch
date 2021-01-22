26289aac58f79f436ad69bd894759d96e329c49b
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-22 07:53:59 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -57,5 +57,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login when invalid credentials are provided', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'danielotavianobp@gmail.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
