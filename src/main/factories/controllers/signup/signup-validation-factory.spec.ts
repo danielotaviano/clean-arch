c4e5cd10e9cf0a6721b76bd3541321999328ab33
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-22 13:18:39 -0300

    refactor: separete factories in folders

--- src/main/factories/controllers/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.spec.ts
@@ -1,9 +1,9 @@
-import { RequiredFieldValidation, CompareFieldsValidation, EmailValidation, ValidationComposite } from '../../../presentation/helpers/validators'
-import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { Validation } from '../../../presentation/protocols/validation'
-import { makeSignUpValidation } from './signup-validation-factory'
 
-jest.mock('../../../presentation/helpers/validators/validation-composite')
+import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from '../../../../presentation/helpers/validators'
+import { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { makeSignUpValidation } from './signup-validation-factory'
+jest.mock('../../../../presentation/helpers/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
