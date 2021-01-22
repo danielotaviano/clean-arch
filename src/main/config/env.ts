df7a918ab0498b8e86f38b3e84ae74d963ccf47f
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-22 09:07:26 -0300

    fix: mongo url config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'FE37FBF7FBBD5B49C1CC9A5BD1B240D2F7E17330E5D38425CEB6B3D93F64FE42'
 }
