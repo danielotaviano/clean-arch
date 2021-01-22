062931183252321e4ce6a695cdfbca98da6b29ac
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-21 21:03:39 -0300

    refactor: rename load with loadByEmail

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -25,7 +25,7 @@ export class DbAuthentication implements Authentication {
   }
 
   async auth({ email, password }: AuthRequest): Promise<string> {
-    const account = await this.loadAccountByEmailRepository.load(email)
+    const account = await this.loadAccountByEmailRepository.loadByEmail(email)
     if (!account) return null
 
     const isValidPassword = await this.hashCompare.compare(password, account.password)
