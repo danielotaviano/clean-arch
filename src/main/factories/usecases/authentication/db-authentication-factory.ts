23ba3547ada5a0cf32af7aa402cd9706c4cda9ba
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-22 13:48:26 -0300

    refactor: move files to correct folders

--- src/main/factories/usecases/authentication/db-authentication-factory.ts
+++ src/main/factories/usecases/authentication/db-authentication-factory.ts
@@ -1,4 +1,4 @@
-import { DbAuthentication } from '../../../../data/usecases/add-account/authentication/db-authentication'
+import { DbAuthentication } from '../../../../data/usecases/authentication/db-authentication'
 import { Authentication } from '../../../../domain/usecases/authentication'
 import { BcryptAdapter } from '../../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '../../../../infra/cryptography/jwt-adapter/jwt-adapter'
