16e31154f35ccca001ebe2fcc1ca6b4b375eb52f
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-23 17:53:40 -0300

    refactor: organize main layer folders

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
-import { AdaptRoute } from '../adapters/express/express-route-adapter'
-import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
-import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
+import { AdaptRoute } from '../adapters/express-route-adapter'
+import { makeLoginController } from '../factories/controllers/login/login/login-controller-factory'
+import { makeSignUpController } from '../factories/controllers/login/signup/signup-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', AdaptRoute(makeSignUpController()))
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { AdaptRoute } from '../adapters/express/express-route-adapter'
-import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
+import { AdaptRoute } from '../adapters/express-route-adapter'
+import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 
 export default (router: Router): void => {
   router.post('/surveys', AdaptRoute(makeAddSurveyController()))
