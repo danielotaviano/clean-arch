32d5fc5005afe4d517dea613bf47452bfb59fc93
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-23 00:18:59 -0300

    feat: add AddSurvey composition

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,48 @@
+/* eslint-disable jest/expect-expect */
+import { Collection } from 'mongodb'
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveyCollection: Collection
+
+describe('Survey Routes', () => {
+  const mongod = new MongoMemoryServer()
+
+  beforeAll(async () => {
+    const uri = await mongod.getUri()
+
+    await MongoHelper.connect(uri)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+    await mongod.stop()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  describe('POST /surveys', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answers: [{
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            }, {
+              answer: 'Answer 2'
+            }]
+          }
+          ]
+        })
+        .expect(204)
+    })
+  })
+})
