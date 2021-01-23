90131ba79ebd1479e5f56d6f0ae5d5b6f7b7ab3d
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-23 18:22:14 -0300

    feat: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { AdaptRoute } from '../adapters/express-route-adapter'
+import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeLoginController } from '../factories/controllers/login/login/login-controller-factory'
 import { makeSignUpController } from '../factories/controllers/login/signup/signup-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/signup', AdaptRoute(makeSignUpController()))
-  router.post('/login', AdaptRoute(makeLoginController()))
+  router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
-import { AdaptRoute } from '../adapters/express-route-adapter'
+import { adaptMiddleware } from '../adapters/express-middleware-adapt'
+import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
-  router.post('/surveys', AdaptRoute(makeAddSurveyController()))
+  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
 }
