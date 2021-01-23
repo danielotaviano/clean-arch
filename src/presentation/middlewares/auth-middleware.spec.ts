64e98ce25cf30d0db7f9de50b959cda1010343f3
Daniel Otaviano <danielpontes0@icloud.com>
2021-01-23 15:24:22 -0300

    refactor: move AuthMiddleware dependencies to a protocols file

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,9 +2,8 @@
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
 import { AccountModel } from '../../domain/models/account'
-import { HttpRequest } from '../protocols'
+import { HttpRequest, LoadAccountByToken } from './auth-middleware-protocols'
 
 const makeFakeAccount = ():AccountModel => ({
   email: 'any_email@mail.com',
